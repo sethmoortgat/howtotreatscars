9d5edfada83d30fdb5f895eb5b2370b1e43e1a1a
Seth Moortgat <sethmoortgat@MacBook-Pro-van-Seth.local>
2024-02-11 22:14:25 +0100

    initial interface

--- interface.py
+++ interface.py
@@ -1,28 +1,14 @@
+from langchain_community.vectorstores import Chroma
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_openai import OpenAI, ChatOpenAI
+
 import streamlit as st
-from streamlit.components.v1 import iframe
+import os
+import openai
+import time
 import hmac
 
-from langchain.prompts import ChatPromptTemplate
-# import tiktoken
-
-import string
-import random
-import base64
-import time
-import openai
-import os
-import requests
-import json
-
-def randon_string() -> str:
-    return "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
-
-
-from openai.error import RateLimitError
-# from dotenv import load_dotenv, find_dotenv
-# _ = load_dotenv(find_dotenv()) # read local .env file
-
-# token_counter = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 openai.api_key  = os.environ['OPENAI_API_KEY']
@@ -30,38 +16,35 @@ openai.api_key  = os.environ['OPENAI_API_KEY']
 
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo-1106", temperature=0):
-    """
-    Input takes a n array of messages that specify a set of prompts and associated roles
-    for example:
-    messages =  [  
-        {'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
-        {'role':'user', 'content':'tell me a joke'},   
-        {'role':'assistant', 'content':'Why did the chicken cross the road'},   
-        {'role':'user', 'content':'I don\'t know'}  
-    ]
-    
-    Output is a message that has a format of: [{"role": "user", "content": prompt}].
-    One needs to use the parse_response function to extract the actual 
-    model output message from the output.
-    """
-    try:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            temperature=temperature, # this is the degree of randomness of the model's output
-        )
-        
-        # This is how you can get the number of tokens from the response
-        token_dict = {
-        'prompt_tokens':response['usage']['prompt_tokens'],
-        'completion_tokens':response['usage']['completion_tokens'],
-        'total_tokens':response['usage']['total_tokens'],
-            }
+	"""
+	Input takes a n array of messages that specify a set of prompts and associated roles
+	for example:
+	messages =  [  
+		{'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
+		{'role':'user', 'content':'tell me a joke'},   
+		{'role':'assistant', 'content':'Why did the chicken cross the road'},   
+		{'role':'user', 'content':'I don\'t know'}  
+	]
 
-    except RateLimitError as err: 
-        return err
-    
-    return response.choices[0].message # , token_dict
+	Output is a message that has a format of: [{"role": "user", "content": prompt}].
+	One needs to use the parse_response function to extract the actual 
+	model output message from the output.
+	"""
+
+	response = openai.ChatCompletion.create(
+		model=model,
+		messages=messages,
+		temperature=temperature, # this is the degree of randomness of the model's output
+	)
+
+	# This is how you can get the number of tokens from the response
+	# token_dict = {
+	# 	'prompt_tokens':response['usage']['prompt_tokens'],
+	# 	'completion_tokens':response['usage']['completion_tokens'],
+	# 	'total_tokens':response['usage']['total_tokens'],
+	# 	}
+	#     
+	return response.choices[0].message # , token_dict
 
 
 
@@ -110,61 +93,49 @@ def main():
 					with st.chat_message(message["role"]): st.write(message["content"])
 	
 	
-	def clear_history():
-		st.session_state.messages = [  
-			{'role':'system', 'content':'You are an AI assistent.'},
-			{'role':'assistant', 'content':'Hi there, how can I help you?'},
-		] 
+	def new_question():
+		st.session_state.new_question = True
 	
+	def existing_question():
+		st.session_state.new_question = False
 	
+	if "new_question" not in st.session_state.keys():
+		st.session_state.new_question = True
+	
+	if "vector_store" not in st.session_state.keys():
+		st.session_state.vectorstore = Chroma(persist_directory="./chroma_db")
 		
-	st.sidebar.button("Clear History...", on_click=clear_history)
-	
-	st.sidebar.slider(
-    	'Temperature:',
-    	0.0, 1.0, 0.2,
-    	key="temperature_value",
-    	on_change=show_conversation)
+	st.sidebar.button("New question", on_click=new_question)
 	
 	st.sidebar.selectbox(
 	   "Which model do you want to use",
 	   ("gpt-3.5-turbo-1106", "gpt-4", "gpt-4-1106-preview"),
 	   index=0,
 	   key="model_value",
-	   on_change=clear_history,
+	   on_change=new_question,
 	)
 	
-	if "history" not in st.session_state.keys():
-		st.session_state.history = []
-	
-	st.session_state.history.append(st.title("chatGPT"))
-	st.session_state.history.append(st.subheader(f"Powered by ChatGPT version: {st.session_state.model_value}",help=f"{st.session_state.model_value} is a large languange model (LLM) hosted by OpenAI, that can understand as well as generate natural language or code: [link for more info](https://platform.openai.com/docs/models)"))
+	st.title("[HowToTreatScars](https://howtotreatscars.com/)")
+	st.image('./banner2.jpg')
 	
 	
-	if "messages" not in st.session_state.keys():
-		st.session_state.messages = [  
-				{'role':'system', 'content':'You are an AI assistent.'},
-				{'role':'assistant', 'content':'Hi there, how can I help you?'},
-			] 
-	
-	st.session_state.conversation_box = st.container()
-	
-	
-	
-	def submit_question():
-		st.session_state.messages.append({'role':'user', 'content':st.session_state.chat_input})		
-		answer = get_completion_from_messages(st.session_state.messages, temperature = st.session_state.temperature_value, model=st.session_state.model_value)
-		#answer = {'role':'assistant', 'content':randon_string()}
-		st.session_state.messages.append(answer)
-		show_conversation()
-
-	if len(st.session_state.messages)==2:
-		show_conversation()
-	
-	
+	if st.session_state.new_question:	
+		st.button("New question YES", on_click=existing_question)
 		
+	else:
+		st.chat_input("Enter your message", on_submit=new_question, key="chat_input")
+
+		
+	
+	
+	
+	
+	#st.session_state.history.append(st.subheader(f"Powered by ChatGPT version: {st.session_state.model_value}",help=f"{st.session_state.model_value} is a large languange model (LLM) hosted by OpenAI, that can understand as well as generate natural language or code: [link for more info](https://platform.openai.com/docs/models)"))
+	
+	
+	
 			
-	st.chat_input("Enter your message", on_submit=submit_question, key="chat_input")
+	#st.chat_input("Enter your message", on_submit=submit_question, key="chat_input")
 
 	
 	
